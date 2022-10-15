222b797955efd34a0097731d9c8dab1c7e29acf6
Ardian Eka Candra <ardianoption@gmail.com>
2022-10-15 19:29:19 +0700

    outputs should be a function

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "nix-react-native";
-  outputs = {
-    templates.default = {
-      path = ./template;
-      description = "React Native environment for nix";
-    };
+
+  # To update all inputs:
+  # inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = (
+    system
+  ) // {
+    defaultTemplate.path = ./template;
+    defaultTemplate.description = "nix flake new 'github:numtide/devshell'";
   };
 }
